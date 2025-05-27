b4a67da5def4191e4c147aa254773a0f3a119170
Kalkidan-dev <cakek433@gmail.com>
2025-05-27 22:31:23 +0300

    First time

--- frontendn/src/App.js
+++ frontendn/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import { AuthProvider } from './context/AuthContext';
+import { RoleProvider } from './context/RoleContext';
 
-function App() {
+import Home from './pages/Home';
+import CourseList from './pages/CourseList';
+import CourseDetail from './pages/CourseDetail';
+import Navbar from './components/layout/Navbar';
+import Sidebar from './components/layout/Sidebar';
+
+
+import StudentDashboard from './pages/StudentDashboard';
+import InstructorDashboard from './pages/InstructorDashboard';
+import UploadCourse from './pages/UploadCourse';
+import AdminPanel from './pages/AdminPanel';
+import Login from './pages/Login';
+import VoiceControl from './components/VoiceControl';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <RoleProvider>
+        <ThemeProvider>
+          <Router>
+            <div className="app-container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/student-dashboard" element={<StudentDashboard />} />
+                <Route path="/instructor-dashboard" element={<InstructorDashboard />} />
+                <Route path="/navbar" element={<Navbar />} />
+                <Route path="/sidebar" element={<Sidebar />} />
+                <Route path="/" element={<CourseList />} />
+                <Route path="/courses" element={<CourseList />} />
+                <Route path="/courses/:id" element={<CourseDetail />} />
+                <Route path="/upload-course" element={<UploadCourse />} />
+                <Route path="/admin-panel" element={<AdminPanel />} />
+                <Route path="/login" element={<Login />} />
+                {/* Add other routes here */}
+              </Routes>
+
+              {/* VoiceControl listens everywhere */}
+              <VoiceControl />
+            </div>
+          </Router>
+        </ThemeProvider>
+      </RoleProvider>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
