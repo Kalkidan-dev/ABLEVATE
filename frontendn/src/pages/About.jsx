e8a1568b69123b8a9f8c7bc825530bb141859179
KalDev-2 <kalkidangemechudev@gmail.com>
2025-05-30 22:12:09 +0300

    About page created

---
+++ frontendn/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+  <div className="p-6 max-w-3xl mx-auto">
+    <h1 className="text-3xl font-bold mb-4">About ABLEVATE</h1>
+    <p className="text-lg">
+      ABLEVATE is an inclusive e-learning platform designed to empower visually and hearing-impaired students through accessible digital education.
+    </p>
+  </div>
+);
+
+export default About;
