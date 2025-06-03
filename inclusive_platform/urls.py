6035cdb5241d42a12fa5d949bd9578147cf96a5d
Kalkidan-dev <cakek433@gmail.com>
2025-06-03 20:32:40 +0300

    Updated

--- inclusive_platform/urls.py
+++ inclusive_platform/urls.py
@@ -3,8 +3,15 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+   TokenRefreshView,
+)
+from core.accounts.views import LoginView
+
 
 urlpatterns = [
+    path('api/token/', LoginView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/', include('core.accounts.urls')),
     path('api/courses/', include('core.courses.urls')),
