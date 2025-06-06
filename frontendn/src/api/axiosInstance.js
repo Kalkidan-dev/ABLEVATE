745fba9418be4cba680eb4e394c7bcb609f452dc
Kalkidan-dev <cakek433@gmail.com>
2025-06-06 22:12:58 +0300

    Updated

--- frontendn/src/api/axiosInstance.js
+++ frontendn/src/api/axiosInstance.js
@@ -1,9 +1,18 @@
-// Example: src/api/axiosInstance.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
-  withCredentials: true,
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosInstance;
