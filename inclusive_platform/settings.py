bc5e042de02decdd2f4ed10a7a904451f94bfba2
Kalkidan-dev <cakek433@gmail.com>
2025-06-10 21:34:49 +0300

    Updated

--- inclusive_platform/settings.py
+++ inclusive_platform/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'django_extensions',
     'core.accounts',
     'core.courses',
     'rest_framework',
